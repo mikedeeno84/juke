f57249763a7aaedc4a7f44458991c9f6f0e2b72d
Sam Narisi <snarisi@gmail.com>
2015-12-02 16:57:33 -0500

    Implement audio controls

--- browser/app.js
+++ browser/app.js
@@ -1,38 +1,39 @@
 var app=angular.module("juke",[])
 
-app.controller('MainControl', function($scope){
-
-	$scope.album = {
-	    name: 'Abbey Road',
-	    imageUrl: 'http://fillmurray.com/300/300',
-	    songs: [{
-	        name: 'Romeo & Juliette',
-	        artists: [{name: 'Bill'}],
-	        genres: ['Smooth', 'Funk'],
-	        audioUrl: 'http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2013.mp3'
-	    }, {
-	        name: 'White Rabbit',
-	        artists: [{name: 'Bill'}, {name: 'Bob'}],
-	        genres: ['Fantasy', 'Sci-fi'],
-	        audioUrl: 'http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2008.mp3'
-	    }, {
-	        name: 'Lucy in the Sky with Diamonds',
-	        artists: [{name: 'Bob'}],
-	        genres: ['Space'],
-	        audioUrl: 'http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2001.mp3'
-	    }]
-	};
+app.controller('MainControl', function($scope, $http){
 
 	$scope.getArtists = function(song){
 		return song.artists.map(function(artist){return artist.name}).join(", ");
 	}
-
-
-
-
-
-
-})
+	$scope.currentSong = '';
+	var audio = document.createElement('audio');
+	//grab list of albums
+	var getAlbum = $http.get('/api/albums/565f2e9f4d3f39cd11d86986');
+	
+	getAlbum.then(function(response) {
+		$scope.album = response.data;
+		$scope.getAlbumImage = function() {
+			return '/api/albums/' + response.data._id.toString() + '.image';	
+		};
+		$scope.playSong = function(songId) {
+			if (!songId || $scope.currentSong === songId) return audio.play();
+				
+			audio.pause();
+			$scope.currentSong = songId;
+			audio.src = '/api/songs/' + songId + '.audio';
+			audio.load();
+			audio.play();
+		}
+		
+		$scope.pauseSong = function() {
+			audio.pause();
+		}
+	});
+	
+	$scope.isPlaying = function() {
+		return !audio.paused;
+	}
+});
 
 app.controller('MainDisplay', function($scope){
 
