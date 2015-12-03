329453037f65cee7f95a4aa61059e79177e714e7
Sam Narisi <snarisi@gmail.com>
2015-12-03 10:02:19 -0500

    Add scrubbing

--- browser/albumControl.js
+++ browser/albumControl.js
@@ -5,12 +5,18 @@ app.controller('AlbumControl', function($scope, $rootScope, $http){
 	}
 	var trackIDs = [];
 
-	var getAlbum = $http.get('/api/albums/565f6c85524e52611be16151');
+	var getAlbum = $http.get('/api/albums/565f2e9f4d3f39cd11d86986');
 
+	//mike: 565f6c85524e52611be16151
+	//sam: 565f2e9f4d3f39cd11d86986
+	
 	getAlbum.then(function(response) {
 		$scope.album = response.data;
 		$scope.album
 		trackIDs = $scope.album.songs.map(function(song){return song._id});
+		
+		$rootScope.$broadcast('albumLoaded', trackIDs);
+		
 		$scope.getAlbumImage = function() {
 			return '/api/albums/' + response.data._id.toString() + '.image';	
 		};
@@ -23,11 +29,16 @@ app.controller('AlbumControl', function($scope, $rootScope, $http){
 		$rootScope.$broadcast('pauseClicked');
 	}
 	$scope.isPlaying = false;
+	$scope.currentSong = '';
 	
-	$rootScope.$on('play',function(eventInfo){
+	$rootScope.$on('play',function(eventInfo, currentSong){
 		$scope.isPlaying = true;
+		$scope.currentSong = currentSong;
+		console.log($scope.currentSong);
+		$scope.$digest();
 	})
 	$rootScope.$on('pause',function(eventInfo){
 		$scope.isPlaying = false;
+		$scope.$digest();
 	})
 });
--- browser/playerControl.js
+++ browser/playerControl.js
@@ -3,7 +3,12 @@ app.controller('PlayerControl', function($scope, $rootScope){
 
 
 	$scope.currentSong = '';
+	var trackIDs;
 	var audio = document.createElement('audio');
+	var shuffleList;
+	var orderedList;
+	$scope.shuffleOn = false;
+	
 	//grab list of albums
 
 	//stuff is in seperate controller
@@ -35,7 +40,7 @@ app.controller('PlayerControl', function($scope, $rootScope){
 	    $scope.$digest();
 		});
 
-	var playSong = function(songId) {
+	$scope.playSong = function(songId) {
 			if (!songId || $scope.currentSong === songId) return audio.play();
 				
 			audio.pause();
@@ -45,23 +50,69 @@ app.controller('PlayerControl', function($scope, $rootScope){
 			audio.play();
 		}
 		
-	var pauseSong = function() {
+	$scope.pauseSong = function() {
+		audio.pause();
+	}
+	
+	$scope.playRandom = function() {
+		if ($scope.shuffleOn) {
+			$scope.shuffleOn = false;
+			trackIDs = orderedList;
 			audio.pause();
+			return;
 		}
+		
+		$scope.shuffleOn = true;
+		var copyOfTrackIDs = trackIDs.slice();
+		var newTrackIndex = [];
+		
+		while (copyOfTrackIDs.length > 0) {
+			var randomIndex = Math.floor(Math.random() * copyOfTrackIDs.length);
+			newTrackIndex.push(copyOfTrackIDs.splice(randomIndex, 1)[0]);
+		}
+		
+		shuffleList = newTrackIndex;
+		trackIDs = shuffleList;
+		
+		$scope.playSong(trackIDs[0]);
+	}
+	
+	$rootScope.$on('albumLoaded', function(eventInfo, incomingTrackIds) {
+		orderedList = incomingTrackIds;
+		trackIDs = orderedList;
+	})
+	
 	$rootScope.$on('playClicked', function(eventInfo, songId){
-	playSong(songId);
+	$scope.playSong(songId);
 	});
 
 	$rootScope.$on('pauseClicked', function(eventInfo){
-	pauseSong();
+	$scope.pauseSong();
 	});
-
-	audio.addEventListener('onplaying', function(){
-		$rootScope.$broadcast('play');
-	})
-	audio.addEventListener('onpause', function(){
+	
+	audio.addEventListener('play', function(){
+		console.log($scope.currentSong);
+		$rootScope.$broadcast('play', $scope.currentSong);
+	});
+	audio.addEventListener('pause', function(){
 		$rootScope.$broadcast('pause');
-	})
+	});
+	audio.addEventListener('ended', function() {
+		$scope.nextSong();
+	});	
+	
+	$scope.progressBarClick = function($event) {
+		var totalWidth;
+		var percentComplete;
+		var targetTime;
+		
+		if ($event.target.className === 'progress') totalWidth = $event.target.clientWidth;
+		else totalWidth = $event.target.parentElement.clientWidth;
+
+		percentComplete = $event.layerX / totalWidth;
+		targetTime = audio.duration * percentComplete;
+		audio.currentTime = targetTime;
+	}
 });
 
 
